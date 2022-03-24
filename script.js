0e6078bfbe14b9cb375a4e17250401af6da6f89f
mochammad-fachorrozi <mochammadfachorrozi98@gmail.com>
2022-03-24 10:24:54 +0700

    menambahkan script dark mode di style.css dan script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// jam
 const judulWib = document.querySelector('#judul-wib');
 judulWib.innerHTML = "Waktu Indonesia Barat";
 
@@ -7,10 +8,19 @@ function waktu() {
     const waktu = new Date();
     setTimeout("waktu()", 1000);
     // jika mau membuat WIT silahkan tambahkan +1 setelah function getHours
-    document.getElementById('jam').innerHTML = waktu.getHours();
-    document.getElementById('menit').innerHTML = waktu.getMinutes();
-    document.getElementById('detik').innerHTML = waktu.getSeconds();
+    const a = document.getElementById('jam').innerHTML = waktu.getHours();
+    const b = document.getElementById('menit').innerHTML = waktu.getMinutes();
+    const c = document.getElementById('detik').innerHTML = waktu.getSeconds();
+
+    // jika diatas jam 5 dan dibawah jam 18 dark-mode tidak tampil, selain itu tampil
+    if ( 5 >= a <= 18 && b > 00) {
+        document.body.classList.remove('dark-mode');
+    } else {
+        document.body.classList.add('dark-mode');
+    }
 }
+// end-jam
+
 
 // Tanggal 
 const judulTanggal = document.querySelector('#judul-tanggal');
@@ -31,4 +41,5 @@ document.getElementById('hari').innerHTML = namaHari[hari];
 document.getElementById('tanggal').innerHTML = tgl;
 document.getElementById('bulan').innerHTML = namaBulan[bulan];
 document.getElementById('tahun').innerHTML = tahun;
+// end-tanggal
 
